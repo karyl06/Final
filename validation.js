b7337057e15a8f9468fc395e78eb8f605406aa71
karyl06 <fabellonkaryl@gmail.com>
2026-05-28 20:43:58 +0800

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,26 +1,18 @@
-```javascript id="vr9l0d"
-// Bootstrap Form Validation
-
 (() => {
     'use strict';
 
-    // Select all forms with validation
     const forms = document.querySelectorAll('.needs-validation');
 
-    // Loop through each form
     Array.from(forms).forEach(form => {
 
         form.addEventListener('submit', event => {
 
-            // If form is invalid
             if (!form.checkValidity()) {
 
-                // Stop submission
                 event.preventDefault();
                 event.stopPropagation();
             }
 
-            // Add Bootstrap validation styles
             form.classList.add('was-validated');
 
         }, false);
